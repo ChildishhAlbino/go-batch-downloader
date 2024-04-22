f124a8e1c12a2f8d75069bea14e3f3865868e3ff
Connor Williams <connorwilliams.home@gmail.com>
2024-04-22 12:48:58 +1000

    fix: cleanup

--- app.go
+++ app.go
@@ -15,71 +15,80 @@ import (
 
 var Mutex = &sync.RWMutex{}
 var newItems = map[string]downloader.DownloadItem{}
+var longestFileName int = 0
 
 func main() {
-    urls := GetUrlsFromFile("./files.txt")
-    numUrls := len(urls)
-    fmt.Println("Found", numUrls, "urls")
-    bufferSize := runtime.NumCPU() * numUrls
-    messages := make(chan downloader.DownloadItem, bufferSize)
+	urls := GetUrlsFromFile("./files.txt")
+	numUrls := len(urls)
+	fmt.Println("Found", numUrls, "urls")
+	bufferSize := runtime.NumCPU() * numUrls
+	messages := make(chan downloader.DownloadItem, bufferSize)
 
-    var wg sync.WaitGroup
-    for _, Url := range urls {
-        parsed, err := url.Parse(Url)
-        core.Check(err)
-        pathSegments := strings.Split(parsed.Path, "/")
-        fileName := pathSegments[len(pathSegments)-1]
-        fmt.Println(fileName)
-        wg.Add(1)
-        go downloader.HandleDownload(Url, fileName, &wg, messages)
-    }
-    for {
-        time.Sleep(20 * time.Millisecond)
-        fmt.Printf("\n----------------------\n")
-        os.Stdout.WriteString("\x1b[3;J\x1b[H\x1b[2J")
-        _, err := downloader.GetDownloads()
-        if(err != nil){
-            fmt.Println("All downloads finished...")
-            break
-        }
-        totalMessages := len(messages)
-        for len(messages) > 0 {
-            msg := <- messages
-            newItems[msg.Url] = msg
-        }
-        sortedKeys := GetSortedListOfKeysFromMap(newItems)
-        fmt.Printf("Downloading %v items... (Processed %v (bufferSize=%v) messages since last update)\n", len(sortedKeys), totalMessages, bufferSize)
-        fmt.Println("-----------------")
-        for _,key := range sortedKeys {
-            item := newItems[key]
-            fmt.Printf("%v    :   %.3fMB OF %.3fMB\n", item.FileName, AsMegabytes(item.BytesDownloaded), AsMegabytes(item.TotalSize))
-        }
-    }
+	var wg sync.WaitGroup
+	for _, Url := range urls {
+		parsed, err := url.Parse(Url)
+		core.Check(err)
+		pathSegments := strings.Split(parsed.Path, "/")
+		fileName := pathSegments[len(pathSegments)-1]
+		length := len(fileName)
+		if length > longestFileName {
+			longestFileName = length
+		}
+		fmt.Println(fileName)
+		wg.Add(1)
+		go downloader.HandleDownload(Url, fileName, &wg, messages)
+	}
+	for {
+		time.Sleep(20 * time.Millisecond)
+		fmt.Printf("\n----------------------\n")
+		os.Stdout.WriteString("\x1b[3;J\x1b[H\x1b[2J")
+		_, err := downloader.GetDownloads()
+		if err != nil {
+			fmt.Println("All downloads finished...")
+			break
+		}
+		totalMessages := len(messages)
+		for len(messages) > 0 {
+			msg := <-messages
+			if msg.BytesDownloaded == msg.TotalSize {
+				delete(newItems, msg.Url)
+				continue
+			}
+			newItems[msg.Url] = msg
+		}
+		sortedKeys := GetSortedListOfKeysFromMap(newItems)
+		fmt.Printf("Downloading %v items... (Processed %v (bufferSize=%v) messages since last update)\n", len(sortedKeys), totalMessages, bufferSize)
+		fmt.Println("-----------------")
+		for _, key := range sortedKeys {
+			item := newItems[key]
+			fmt.Printf("%-*s: %.3fMB OF %.3fMB\n", longestFileName+2, item.FileName, AsMegabytes(item.BytesDownloaded), AsMegabytes(item.TotalSize))
+		}
+	}
 
-    wg.Wait()
-    close(messages)
-    fmt.Println("Finished")
+	wg.Wait()
+	close(messages)
+	fmt.Println("Finished")
 }
 
 func AsMegabytes(bytes int64) float64 {
-    return float64(float64(bytes) / (1024 * 1024))
+	return float64(float64(bytes) / (1024 * 1024))
 }
 
 func GetUrlsFromFile(path string) []string {
-    dat, err := os.ReadFile(path)
-    core.Check(err)
-    asText := string(dat)
-    lines := strings.Fields(asText)
-    return lines
+	dat, err := os.ReadFile(path)
+	core.Check(err)
+	asText := string(dat)
+	lines := strings.Fields(asText)
+	return lines
 }
 
 func GetSortedListOfKeysFromMap(mapToSort map[string]downloader.DownloadItem) []string {
-    keys := make([]string, 0, len(mapToSort))
+	keys := make([]string, 0, len(mapToSort))
 
-    for k := range mapToSort{
-        keys = append(keys, k)
-    }
-    sort.Strings(keys)
+	for k := range mapToSort {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
 
-    return keys    
+	return keys
 }
--- core/downloader/downloader.go
+++ core/downloader/downloader.go
@@ -12,63 +12,68 @@ import (
 
 var downloads map[string]DownloadItem = make(map[string]DownloadItem)
 var Mutex = &sync.RWMutex{}
+
 type ProgressReader struct {
-    Reader io.Reader
-    Size   int64
-    Pos    int64
+	Reader io.Reader
+	Size   int64
+	Pos    int64
 	Url    string
-    Ch     chan<-DownloadItem
+	Ch     chan<- DownloadItem
 }
 
 func (pr *ProgressReader) Read(p []byte) (int, error) {
-    n, err := pr.Reader.Read(p)
-    if err == nil {
-        pr.Pos += int64(n)
+	n, err := pr.Reader.Read(p)
+	shouldProcess := err == nil || err.Error() == "EOF"
+	if shouldProcess {
+		pr.Pos += int64(n)
 		existing := downloads[pr.Url]
-        existing.BytesDownloaded = pr.Pos
-        existing.TotalSize = pr.Size
-        pr.Ch <- existing
-    }
-    return n, err
+		existing.BytesDownloaded = pr.Pos
+		existing.TotalSize = pr.Size
+		pr.Ch <- existing
+	} else {
+		fmt.Println(err)
+	}
+	return n, err
 }
 
 func GetDownloads() (map[string]DownloadItem, error) {
-	if(len(downloads) == 0){
+	if len(downloads) == 0 {
 		return nil, errors.New("all downloads are closed")
 	}
 	return downloads, nil
 }
 
-func HandleDownload(url string, outName string, wg *sync.WaitGroup, ch chan<-DownloadItem) (error) {
+func HandleDownload(url string, outName string, wg *sync.WaitGroup, ch chan<- DownloadItem) error {
 	Mutex.Lock()
 	downloads[url] = DownloadItem{FileName: outName, Url: url}
 	Mutex.Unlock()
 	defer wg.Done()
-    tempPath := fmt.Sprintf(".tmp_%s", outName)
+	tempPath := fmt.Sprintf(".tmp_%s", outName)
 	fmt.Println("Downloading to temporary path", tempPath)
-    req, _ := http.NewRequest("GET", url, nil)
-    resp, _ := http.DefaultClient.Do(req)
-    if resp.StatusCode != 200 {
-        log.Fatalf("Error while downloading: %v", resp.StatusCode)
-    }
-    defer resp.Body.Close()
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, _ := http.DefaultClient.Do(req)
+	if resp.StatusCode != 200 {
+		log.Fatalf("Error while downloading: %v", resp.StatusCode)
+	}
+	defer resp.Body.Close()
 
-    f, _ := os.OpenFile(tempPath, os.O_CREATE|os.O_WRONLY, 0644)
-    defer f.Close()
+	f, _ := os.OpenFile(tempPath, os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
 
-    progressReader := &ProgressReader{
-        Reader: resp.Body,
-        Size:   resp.ContentLength,
-		Url: 	url,
-        Ch:     ch,
-    }
+	progressReader := &ProgressReader{
+		Reader: resp.Body,
+		Size:   resp.ContentLength,
+		Url:    url,
+		Ch:     ch,
+	}
 
-    if _, err := io.Copy(f, progressReader); err != nil {
-        log.Fatalf("Error while downloading: %v", err)
+	if _, err := io.Copy(f, progressReader); err != nil {
+		log.Fatalf("Error while downloading: %v", err)
 		return err
-    }
-
-    os.Rename(tempPath, outName)
+	}
+	fmt.Println("Renaming file")
+	os.Rename(tempPath, outName)
+	fmt.Println("Renamed file")
 	Mutex.Lock()
 	delete(downloads, url)
 	Mutex.Unlock()
@@ -76,8 +81,8 @@ func HandleDownload(url string, outName string, wg *sync.WaitGroup, ch chan<-Dow
 }
 
 type DownloadItem struct {
-    FileName string
-	Url string
-    BytesDownloaded  int64
-	TotalSize int64
+	FileName        string
+	Url             string
+	BytesDownloaded int64
+	TotalSize       int64
 }
